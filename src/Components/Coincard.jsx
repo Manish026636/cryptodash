9853d1d46d5da33355756d080db881dda9481c0a
Manish <my79220@gmail.com>
2023-08-27 22:11:56 +0530

    hey

--- src/Components/Coincard.jsx
+++ src/Components/Coincard.jsx
@@ -26,10 +26,10 @@ const coinData = [
 const CoinCard = ({ title, imageUrl, price }) => (
   <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
     <div
-      className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md transition duration-200 group-hover:scale-110"
+      className="w-full image-zoom  h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md transition duration-200 group-hover:scale-110"
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
-    <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+    <div className="w-56  -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
       <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
         {title}
       </h3>
@@ -48,12 +48,12 @@ const Coincard = () => (
     <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
       <div className="mb-6 flex items-end justify-between gap-4">
         <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">Market Trends</h2>
-        <a
-          href="#"
+        <button
+         
           className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
         >
           Show more
-        </a>
+        </button>
       </div>
       <div className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
         {coinData.map((coin, index) => (
