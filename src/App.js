2d8057cda561473cbcc346aa1f540aa4e99f39cc
Manish <my79220@gmail.com>
2023-08-27 16:05:00 +0530

    hey

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Footer from './Components/Footer';
+import Coincard from './Components/Coincard';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Coincard/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
