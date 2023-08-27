36ce74ac3413539193a399daa604f9d26f0d5191
Manish <my79220@gmail.com>
2023-08-27 21:30:44 +0530

    hey

--- src/Components/Coincard.jsx
+++ src/Components/Coincard.jsx
@@ -1,138 +1,67 @@
-import React from 'react'
+import React from 'react';
 
-const Coincard = () => {
-  return (
-    <div>
-        <div class="bg-white ">
-  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-    <div class="mb-6 flex items-end justify-between gap-4">
-      <h2 class="text-2xl font-bold text-gray-800 lg:text-3xl">Market Trends</h2>
+const coinData = [
+  {
+    title: 'Bitcoin',
+    imageUrl: 'https://images.unsplash.com/photo-1651055678337-bbe6661e5d92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80',
+    price: 129
+  },
+  {
+    title: 'USDT',
+    imageUrl: 'https://images.unsplash.com/photo-1651055693398-0d66969cf759?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80',
+    price: 129
+  },
+  {
+    title: 'BNB',
+    imageUrl: 'https://images.unsplash.com/photo-1651055699023-36d26d793a21?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80',
+    price: 129
+  },
+  {
+    title: 'LUNA',
+    imageUrl: 'https://images.unsplash.com/photo-1651055749491-7be14d8a3ef0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80',
+    price: 129
+  }
+];
 
-      <a href="#" class="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">Show more</a>
-    </div>
-
-    <div class="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Rachit Tank" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-
-          <span class="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">sale</span>
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Timely Watch</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$15.00</span>
-            <span class="mb-0.5 text-red-500 line-through">$30.00</span>
-          </div>
-        </div>
-      </div>
-    
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1485955900006-10f4d324d411?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Galina N" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Fancy Plant</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$9.00</span>
-          </div>
-        </div>
-      </div>
-    
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by eniko kis" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Elderly Cam</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$45.00</span>
-          </div>
-        </div>
-      </div>
-    
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1560343090-f0409e92791a?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Irene Kredenets" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Shiny Shoe</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$29.00</span>
-          </div>
-        </div>
-      </div>
-     
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1528476513691-07e6f563d97f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Charles Deluvio" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Spiky Plant</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$4.00</span>
-          </div>
-        </div>
-      </div>
-    
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1612033448550-9d6f9c17f07d?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Fernando Lavin" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Wieldy Film</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$19.00</span>
-          </div>
-        </div>
-      </div>
-   
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1579609598065-79f8e5bcfb70?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Kiran CK" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          <span class="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">sale</span>
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Sturdy Stand</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$12.00</span>
-            <span class="mb-0.5 text-red-500 line-through">$24.00</span>
-          </div>
-        </div>
-      </div>
-     
-      <div>
-        <a href="#" class="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
-          <img src="https://images.unsplash.com/photo-1619066045029-5c7e8537bd8c?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Fakurian Design" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div>
-          <a href="#" class="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Lazy Bottle</a>
-
-          <div class="flex items-end gap-2">
-            <span class="font-bold text-gray-800 lg:text-lg">$9.00</span>
-          </div>
-        </div>
+const CoinCard = ({ title, imageUrl, price }) => (
+  <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+    <div
+      className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md transition duration-200 group-hover:scale-110"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+      <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+        {title}
+      </h3>
+      <div className="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+        <span className="font-bold text-gray-800 dark:text-gray-200">${price}</span>
+        <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">
+          Read more
+        </button>
       </div>
     </div>
   </div>
-</div>
-    </div>
-  )
-}
+);
 
-export default Coincard
+const Coincard = () => (
+  <div className="bg-white">
+    <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+      <div className="mb-6 flex items-end justify-between gap-4">
+        <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">Market Trends</h2>
+        <a
+          href="#"
+          className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
+        >
+          Show more
+        </a>
+      </div>
+      <div className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
+        {coinData.map((coin, index) => (
+          <CoinCard key={index} {...coin} />
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default Coincard;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="pb-6 bg-gray-50 lg:pb-0">
+    <header className="pb-6 bg-white lg:pb-0">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <nav className="flex items-center justify-between h-16 lg:h-20">
           <div className="flex-shrink-0">
